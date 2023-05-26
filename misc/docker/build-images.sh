93b052b3fdeec085bd713aee88218f1c757426ed
Iuri Diniz <iuridiniz@gmail.com>
2023-05-26 04:38:22 -0300

    use buildx

--- misc/docker/build-images.sh
+++ misc/docker/build-images.sh
@@ -10,12 +10,12 @@ cd "$BASEDIR"
 for dockerfile in Dockerfile.arm32v7-raspbian:* Dockerfile.arm32v7-steamlink:*; do
     [ -f "$dockerfile" ] || continue
     image_name="`echo "$dockerfile" | sed 's/Dockerfile\.\(.*\)/\1/'`"
-    docker build --progress=plain --platform linux/arm/v7 -t "$DOCKER_IMAGE_BASE/$image_name" -f "$dockerfile" .
+    docker buildx build --load --progress=plain --platform linux/arm/v7 -t "$DOCKER_IMAGE_BASE/$image_name" -f "$dockerfile" .
 done
 
 # 64bits images
 for dockerfile in Dockerfile.arm64v8-*; do
     [ -f "$dockerfile" ] || continue
     image_name="`echo "$dockerfile" | sed 's/Dockerfile\.\(.*\)/\1/'`"
-    docker build --progress=plain --platform linux/arm64 -t "$DOCKER_IMAGE_BASE/$image_name" -f "$dockerfile" .
+    docker buildx build --load --progress=plain --platform linux/arm64 -t "$DOCKER_IMAGE_BASE/$image_name" -f "$dockerfile" .
 done
